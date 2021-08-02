b23ee21768dd9f072f59067bae0136e863585d6d
Davoyandun <yandundavid@gmail.com>
2021-08-02 17:37:43 -0500

    enlace db y llamada a la api

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,6 +1,13 @@
 const { DataTypes } = require('sequelize');
+const axios = require('axios')
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+const {
+  API_KEY
+} = process.env;
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
@@ -8,5 +15,20 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id:{
+      type : DataTypes.ID,
+      
+    }
   });
 };
+
+
+
+
+
+
+
+
+axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`).then((e)=>
+  console.log(e)
+)
