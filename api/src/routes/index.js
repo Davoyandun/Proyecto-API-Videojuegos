2352791e65ff0b342d201828aba81a29a279cbc7
Davoyandun <yandundavid@gmail.com>
2021-08-05 12:21:09 -0500

    Avances en las rutas, llamada a la api y a la DB

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,8 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const axios = require ('axios')
+const axios = require("axios");
 
 const router = Router();
 
@@ -26,26 +26,57 @@ En una primera instancia deberán traerlos desde rawg y guardarlos en su propia 
 Recibe los datos recolectados desde el formulario controlado de la ruta de creación de videojuego por body
 Crea un videojuego en la base de datos*/
 
+const infoApi = async () => {
+  try {
+    let info = await axios.get(
+      "https://api.rawg.io/api/games?key=0377e95fcacd4286ab3a097c4fd9fd2b"
+    );
+    let gamesInfo = await info.data.results.map((e) => {
+      return {
+        name: e.name,
+        id: e.id,
+        released: e.released,
+        img: e.background_image,
+        rating: e.rating,
+        generes: e.genres.map((e) => {
+          return {
+            genere: e.name,
+          };
+        }),
+        platforms: e.platforms.map((e) => {
+          return {
+            platform: e.name,
+          };
+        }),
+      };
+    });
+    return gamesInfo;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-const infoApi = async ()=>{
+
+
+
+let infoDB = async ()=>{
     try{
- let info =  await axios.get('https://api.rawg.io/api/games?key=0377e95fcacd4286ab3a097c4fd9fd2b')
-    return console.log (info.data.results)
-
-
-
-
-    } catch (e){
-
+            let gamesDb = await Videogame.findAll(
+      {  includes :{
+          model: Generes,
+          attributes: ['name'],
+          through : {
+              attibutes: [],
+            }
+        }
+        });
+    } catch(e){
         console.log (e)
-
     }
-   
+    
+
+
 
 }
-infoApi()
- 
-
-
 
 module.exports = router;
